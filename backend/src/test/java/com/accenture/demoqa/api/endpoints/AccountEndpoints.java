8920440a857d0b61f672f219b36a65855ebc969c
brunopsantiago <bpsantiagu@gmail.com>
2025-09-29 19:28:08 -0300

    Update AccountEndpoints.java

--- backend/src/test/java/com/accenture/demoqa/api/endpoints/AccountEndpoints.java
+++ backend/src/test/java/com/accenture/demoqa/api/endpoints/AccountEndpoints.java
@@ -4,5 +4,5 @@ public class AccountEndpoints {
     public static final String CREATE_USER = "/Account/v1/User";
     public static final String GENERATE_TOKEN = "/Account/v1/GenerateToken";
     public static final String AUTHORIZED = "/Account/v1/Authorized";
-    public static final String USER_DETAILS = "/Account/v1/User/{userId}"; // Correto: userId
+    public static final String USER_DETAILS = "/Account/v1/User/{userId}";
 }
